098319fba2f2ba1b8afc1b38e04dc79b090e022a
Greg Nazario <greg@gnazar.io>
2023-07-17 14:06:13 -0700

    Add more buttons / cleanup alignment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -100,6 +100,113 @@ function App(this: any) {
             })
     }
 
+    const testError = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_error`,
+                type_arguments: [],
+                arguments: [],
+            })
+    }
+
+    const testU8 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u8`,
+                type_arguments: [],
+                arguments: [
+                    255
+                ],
+            })
+    }
+
+    const testU16 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u16`,
+                type_arguments: [],
+                arguments: [
+                    65535
+                ],
+            })
+    }
+
+    const testU32 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u32`,
+                type_arguments: [],
+                arguments: [
+                    4294967295
+
+                ],
+            })
+    }
+
+    const testU64 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u64`,
+                type_arguments: [],
+                arguments: [
+                    "18446744073709551615"
+                ],
+            })
+    }
+
+    const testU128 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u128`,
+                type_arguments: [],
+                arguments: [
+                    "340282366920938463463374607431768211455"
+                ],
+            })
+    }
+
+    const testU256 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_u256`,
+                type_arguments: [],
+                arguments: [
+                    "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+                ],
+            })
+    }
+
+    const testF32 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_f32`,
+                type_arguments: [],
+                arguments: [
+                    0
+                ],
+            })
+    }
+
+    const testF64 = async () => {
+        await runTransaction(
+            {
+                type: "entry_function_payload",
+                function: `${MODULE_ADDRESS}::wallet_tester::test_f64`,
+                type_arguments: [],
+                arguments: [
+                    0
+                ],
+            })
+    }
+
     const runTransaction = async <T extends Types.TransactionPayload>(payload: T) => {
         console.log(`Running payload: ${payload}`);
         try {
@@ -139,12 +246,44 @@ function App(this: any) {
             }
             {connected && isDevnet() &&
                 <Layout>
-                    <EasyButton msg="Test object (Option<T>)" func={() => testObject()}/>
-                    <EasyButton msg="Test vector object (vector<Option<T>>)" func={() => testVectorObject()}/>
-                    <EasyButton msg="Test option Some (Option<u64>(some))" func={() => testOptionSome()}/>
-                    <EasyButton msg="Test option None (Option<u64>(none))" func={() => testOptionNone()}/>
-                    <EasyButton msg="Test vector option Some (Vector<Option<u64>>(some))" func={() => testOptionVectorSome()}/>
-                    <EasyButton msg="Test vector option None (Vector<Option<u64>>(none))" func={() => testOptionVectorNone()}/>
+                    <EasyTitle msg="Numbers"/>
+                    <Row align="middle">
+                        <Col offset={2}></Col>
+                        <EasyButton msg="Test u8" func={() => testU8()}/>
+                        <EasyButton msg="Test u16" func={() => testU16()}/>
+                        <EasyButton msg="Test u32" func={() => testU32()}/>
+                        <EasyButton msg="Test u64" func={() => testU64()}/>
+                        <EasyButton msg="Test u128" func={() => testU128()}/>
+                        <EasyButton msg="Test u256" func={() => testU256()}/>
+                    </Row>
+
+                    <EasyTitle msg="Fixed Point"/>
+                    <Row align="middle">
+                        <Col offset={2}></Col>
+                        <EasyButton msg="Test f32" func={() => testF32()}/>
+                        <EasyButton msg="Test f64" func={() => testF64()}/>
+                    </Row>
+                    <EasyTitle msg="Objects"/>
+                    <Row align="middle">
+                        <Col offset={2}></Col>
+                        <EasyButton msg="Test object (Object<T>)" func={() => testObject()}/>
+                        <EasyButton msg="Test vector object (vector<Object<T>>)" func={() => testVectorObject()}/>
+                    </Row>
+                    <EasyTitle msg="Options"/>
+                    <Row align="middle">
+                        <Col offset={2}></Col>
+                        <EasyButton msg="Test option Some (Option<u64>(some))" func={() => testOptionSome()}/>
+                        <EasyButton msg="Test option None (Option<u64>(none))" func={() => testOptionNone()}/>
+                        <EasyButton msg="Test vector option Some (Vector<Option<u64>>(some))"
+                                    func={() => testOptionVectorSome()}/>
+                        <EasyButton msg="Test vector option None (Vector<Option<u64>>(none))"
+                                    func={() => testOptionVectorNone()}/>
+                    </Row>
+                    <EasyTitle msg="Errors"/>
+                    <Row align="middle">
+                        <Col offset={2}></Col>
+                        <EasyButton msg="Test error" func={() => testError()}/>
+                    </Row>
                 </Layout>
             }
         </>
@@ -152,15 +291,22 @@ function App(this: any) {
 }
 
 function EasyButton(props: { msg: string, func: () => Promise<void> }) {
+    return <Col>
+        <Button
+            onClick={props.func}
+            type="primary"
+            style={{height: "40px", backgroundColor: "#3f67ff"}}
+        >
+            {props.msg}
+        </Button>
+    </Col>
+        ;
+}
+
+function EasyTitle(props: { msg: string }) {
     return <Row align="middle">
         <Col offset={2}>
-            <Button
-                onClick={props.func}
-                type="primary"
-                style={{height: "40px", backgroundColor: "#3f67ff"}}
-            >
-                {props.msg}
-            </Button>
+            <h2>{props.msg}</h2>
         </Col>
     </Row>;
 }
--- move/sources/tester.move
+++ move/sources/tester.move
@@ -2,17 +2,56 @@ module deploy_account::wallet_tester {
 
     use aptos_framework::object::Object;
     use std::option::Option;
+    use std::fixed_point32::FixedPoint32;
+    use aptos_std::fixed_point64::FixedPoint64;
+
+    /// This is a test error
+    const ETEST_ERROR: u64 = 12345;
 
     public entry fun test_object<T>(_input: Object<T>) {
         // DO nothing
     }
+
     public entry fun test_vector_object<T>(_input: vector<Object<T>>) {
         // DO nothing
     }
+
     public entry fun test_vector_option(_input: Option<u64>) {
         // DO nothing
     }
+
     public entry fun test_option_vector(_input: Option<vector<u64>>) {
         // DO nothing
+    }
+
+    public entry fun test_error() {
+        abort(ETEST_ERROR)
+    }
+
+    public entry fun test_u8(_input: u8) {
+        // Do nothing
+    }
+    public entry fun test_u16(_input: u16) {
+        // Do nothing
+    }
+    public entry fun test_u32(_input: u32) {
+        // Do nothing
+    }
+    public entry fun test_u64(_input: u64) {
+        // Do nothing
+    }
+    public entry fun test_u128(_input: u128) {
+        // Do nothing
+    }
+    public entry fun test_u256(_input: u256) {
+        // Do nothing
+    }
+
+    public entry fun test_f32(_input: FixedPoint32) {
+        // Do nothing
+    }
+
+    public entry fun test_f64(_input: FixedPoint64) {
+        // Do nothing
     }
 }
