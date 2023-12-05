bce45c591454a66906657105102926e79132913f
Greg Nazario <greg@gnazar.io>
2023-12-05 15:08:11 -0800

    Update identity connect config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -29,7 +29,7 @@ import { ShadowWallet } from "@flipperplatform/wallet-adapter-plugin";
 import { FaceWallet } from "@haechi-labs/face-aptos-adapter-plugin";
 import { Face, Network as FaceNetwork } from "@haechi-labs/face-sdk";
 
-const icDappId = "12346ba8-b4e1-4ddf-9c59-3b406b5b5e2a";
+const icDappId = "2c2fc468-afb6-4b9d-98da-8067cdd03b17";
 const FACE_WALLET_TEST_API_KEY =
   "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCS23ncDS7x8nmTuK1FFN0EfYo0vo6xhTBMBNWVbQsufv60X8hv3-TbAQ3JIyMEhLo-c-31oYrvrQ0G2e9j8yvJYEUnLuE-PaABo0y3V5m9g_qdTB5p9eEfqZlDrcUl1zUr4W7rJwFwkTlAFSKOqVCPnm8ozmcMyyrEHgl2AbehrQIDAQAB";
 
