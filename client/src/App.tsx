b34303f42cb30c94e12c2ae31885e8ef40acf499
Greg Nazario <greg@gnazar.io>
2025-01-29 02:00:04 -0500

    more updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,103 +1,117 @@
-import { Alert, Button, Col, Layout, Row } from "antd";
-import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import {Alert, Button, Col, Layout, Row} from "antd";
+import {WalletSelector} from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import {
-  InputTransactionData,
-  useWallet,
+    InputTransactionData,
+    useWallet,
 } from "@aptos-labs/wallet-adapter-react";
-import { useState } from "react";
-import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import {useState} from "react";
+import {AccountAddress, Aptos, AptosConfig, Network} from "@aptos-labs/ts-sdk";
 
 // TODO: Replace these accordingly
 const MODULE_ADDRESS =
-  "0x11af14ee183a29c48be309b1955f2c428501b11f288d5b71f271156bb95ebf91";
+    "0x11af14ee183a29c48be309b1955f2c428501b11f288d5b71f271156bb95ebf91";
 const MODULE_NAME = "only_on_aptos";
 const FUNCTION_NAME = "create_collection";
 const FUNCTION = `${MODULE_ADDRESS}::${MODULE_NAME}::${FUNCTION_NAME}`;
 
-const client = new Aptos(new AptosConfig({ network: Network.MAINNET }));
+const client = new Aptos(new AptosConfig({network: Network.MAINNET}));
 
 function App(props: { expectedNetwork: Network }) {
-  const { network, connected, signAndSubmitTransaction } = useWallet();
+    const {network, connected, signAndSubmitTransaction} = useWallet();
 
-  const isExpectedNetwork = (): boolean => {
-    return network?.name?.toLowerCase() === props.expectedNetwork;
-  };
+    const isExpectedNetwork = (): boolean => {
+        return network?.name?.toLowerCase() === props.expectedNetwork;
+    };
 
-  const createCollection = async () => {
-    await runTransaction(() => {}, {
-      data: {
-        bytecode:
-          "0xa11ceb0b0700000a0405000107010b080c20102c1f00083c53454c463e5f300161ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff14636f6d70696c6174696f6e5f6d65746164617461090003322e3003322e310000000102",
-        typeArguments: [],
-        functionArguments: [],
-      },
-    });
-  };
+    const createCollection = async () => {
+        await runTransaction(() => {
+        }, {
+            data: {
+                bytecode:
+                    "0xa11ceb0b0700000a0405000107010b080c20102c1f00083c53454c463e5f300161ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff14636f6d70696c6174696f6e5f6d65746164617461090003322e3003322e310000000102",
+                typeArguments: [],
+                functionArguments: [],
+            },
+        });
+    };
 
-  const runTransaction = async (
-    setState: TxnCallback,
-    payload: InputTransactionData,
-  ) => {
-    console.log(`Running payload: ${JSON.stringify(payload)}`);
+    const runScript = async () => {
+        await runTransaction(() => {
+        }, {
+            data: {
+                bytecode:
+                    "0xa11ceb0b0700000a0405000507050b08102010301f02060c0500083c53454c463e5f300162ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff14636f6d70696c6174696f6e5f6d65746164617461090003322e3003322e31000001030b000102",
+                typeArguments: [],
+                functionArguments: [AccountAddress.ONE],
+            },
+        });
+    };
 
-    try {
-      const response = await signAndSubmitTransaction(payload);
-      console.log(`Successfully submitted`);
-      console.log(`Retrieved response: ${JSON.stringify(response)}`);
-      await client.waitForTransaction({ transactionHash: response.hash });
-      console.log(
-        `Successfully committed https://explorer.aptoslabs.com/txn/${response.hash}`,
-      );
-      let txn = (await client.getTransactionByHash({
-        transactionHash: response.hash,
-      })) as any;
-      console.log(`Txn: ${JSON.stringify(txn)}`);
-      setState({ state: "success", msg: `Successful txn ${txn.hash}` });
-      return txn;
-    } catch (error: any) {
-      console.log("Failed to wait for txn" + JSON.stringify(error));
-      setState({
-        state: "error",
-        msg: `Failed txn due to ${JSON.stringify(error)}`,
-      });
-    }
+    const runTransaction = async (
+        setState: TxnCallback,
+        payload: InputTransactionData,
+    ) => {
+        console.log(`Running payload: ${JSON.stringify(payload)}`);
 
-    return undefined;
-  };
+        try {
+            const response = await signAndSubmitTransaction(payload);
+            console.log(`Successfully submitted`);
+            console.log(`Retrieved response: ${JSON.stringify(response)}`);
+            await client.waitForTransaction({transactionHash: response.hash});
+            console.log(
+                `Successfully committed https://explorer.aptoslabs.com/txn/${response.hash}`,
+            );
+            let txn = (await client.getTransactionByHash({
+                transactionHash: response.hash,
+            })) as any;
+            console.log(`Txn: ${JSON.stringify(txn)}`);
+            setState({state: "success", msg: `Successful txn ${txn.hash}`});
+            return txn;
+        } catch (error: any) {
+            console.log("Failed to wait for txn" + JSON.stringify(error));
+            setState({
+                state: "error",
+                msg: `Failed txn due to ${JSON.stringify(error)}`,
+            });
+        }
 
-  return (
-    <>
-      <Layout>
-        <Row align="middle">
-          <Col span={10} offset={2}>
-            <h1>Execute Transaction ({props.expectedNetwork})</h1>
-          </Col>
-          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
-            <WalletSelector />
-          </Col>
-        </Row>
-      </Layout>
-      {!connected && (
-        <Alert
-          message={`Please connect your wallet to ${props.expectedNetwork}`}
-          type="info"
-        />
-      )}
-      {connected && !isExpectedNetwork() && (
-        <Alert
-          message={`Wallet is connected to ${network?.name}.  Please connect to ${props.expectedNetwork}`}
-          type="warning"
-        />
-      )}
-      {connected && isExpectedNetwork() && (
-        <Layout>
-          <EasyTitle msg={`Run ${FUNCTION}`} />
-          <EasyButton msg="Submit" func={createCollection} />
-        </Layout>
-      )}
-    </>
-  );
+        return undefined;
+    };
+
+    return (
+        <>
+            <Layout>
+                <Row align="middle">
+                    <Col span={10} offset={2}>
+                        <h1>Execute Transaction ({props.expectedNetwork})</h1>
+                    </Col>
+                    <Col span={12} style={{textAlign: "right", paddingRight: "200px"}}>
+                        <WalletSelector/>
+                    </Col>
+                </Row>
+            </Layout>
+            {!connected && (
+                <Alert
+                    message={`Please connect your wallet to ${props.expectedNetwork}`}
+                    type="info"
+                />
+            )}
+            {connected && !isExpectedNetwork() && (
+                <Alert
+                    message={`Wallet is connected to ${network?.name}.  Please connect to ${props.expectedNetwork}`}
+                    type="warning"
+                />
+            )}
+            {connected && isExpectedNetwork() && (
+                <Layout>
+                    <EasyTitle msg={`Run ${FUNCTION}`}/>
+                    <EasyButton msg="Submit" func={createCollection}/>
+                    <EasyButton msg="Submit" func={runScript}/>
+                </Layout>
+            )}
+        </>
+    );
 }
 
 type ButtonState = { msg: string; state: ReturnState };
@@ -105,49 +119,49 @@ type ReturnState = "success" | "error" | undefined;
 type TxnCallback = (state: ButtonState) => void;
 
 const toState = (state: ReturnState): "success" | "error" | "info" => {
-  if (state !== undefined) {
-    return state;
-  } else {
-    return "info";
-  }
+    if (state !== undefined) {
+        return state;
+    } else {
+        return "info";
+    }
 };
 
 function EasyButton(props: {
-  msg: string;
-  func: (setState: TxnCallback) => Promise<void>;
+    msg: string;
+    func: (setState: TxnCallback) => Promise<void>;
 }) {
-  const [state, setState] = useState<ButtonState>({
-    msg: "",
-    state: undefined,
-  });
-  return (
-    <Row align="middle">
-      <Col offset={2}>
-        <Button
-          onClick={() => props.func(setState)}
-          type="primary"
-          style={{ height: "40px", backgroundColor: "#3f67ff" }}
-        >
-          {props.msg}
-        </Button>
-      </Col>
-      <Col offset={2}>
-        {state.state && (
-          <Alert type={toState(state.state)} message={state.msg} />
-        )}
-      </Col>
-    </Row>
-  );
+    const [state, setState] = useState<ButtonState>({
+        msg: "",
+        state: undefined,
+    });
+    return (
+        <Row align="middle">
+            <Col offset={2}>
+                <Button
+                    onClick={() => props.func(setState)}
+                    type="primary"
+                    style={{height: "40px", backgroundColor: "#3f67ff"}}
+                >
+                    {props.msg}
+                </Button>
+            </Col>
+            <Col offset={2}>
+                {state.state && (
+                    <Alert type={toState(state.state)} message={state.msg}/>
+                )}
+            </Col>
+        </Row>
+    );
 }
 
 function EasyTitle(props: { msg: string }) {
-  return (
-    <Row align="middle">
-      <Col offset={2}>
-        <h2>{props.msg}</h2>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row align="middle">
+            <Col offset={2}>
+                <h2>{props.msg}</h2>
+            </Col>
+        </Row>
+    );
 }
 
 export default App;
